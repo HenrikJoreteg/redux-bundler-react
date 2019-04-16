8f5b9927b0622c99625aa579afbc07fb581ea972
Odin Hørthe Omdal <odin.omdal@gmail.com>
2019-04-16 21:47:38 +0200

    Compile files

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ var connect = function () {
   }(react.Component));
 
   Connect.contextTypes = CONTEXT_TYPES;
-  Connect.displayName = react.Component.displayName || react.Component.name;
+  Connect.displayName = 'connect(' + (Comp.displayName || Comp.name) + ')';
   return Connect;
 };
 
